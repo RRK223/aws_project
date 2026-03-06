bf430f6ab66f45c3f09b80c0491553d9bbce9e41
RRK223 <khatiwadaranjan45@gmail.com>
2026-03-06 15:07:21 +0000

    ec2 failure correcetd

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,16 +1,3 @@
-# modules/ec2/main.tf
-
-resource "aws_instance" "main" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-
-  tags = {
-    Name = "my-ec2-instance-${count.index + 1}"
-  }
-}
-
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
   description = "Allow inbound traffic to EC2 instance"
@@ -28,5 +15,18 @@ resource "aws_security_group" "ec2_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "main" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "my-ec2-instance-${count.index + 1}"
   }
 }
